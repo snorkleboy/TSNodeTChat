4d28e029da9e6ae94db7c45238673934f9ddd276
Artem Kharshan <akharshan@servicechannel.com>
2019-10-06 13:09:24 -0700

    astash

--- src/backend/http/httpApp.ts
+++ src/backend/http/httpApp.ts
@@ -1,14 +1,51 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const mimeType = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.svg': 'image/svg+xml',
+};
 function httpApp(req, res) {
-    res.end("hello http");
-    // fs.readFile(__dirname + '/index.html',
-    //     function (err, data) {
-    //         if (err) {
-    //             res.writeHead(500);
-    //             return res.end('Error loading index.html');
-    //         }
+    // parse URL
+    const parsedUrl = url.parse(req.url);
 
-    //         res.writeHead(200);
-    //         res.end(data);
-    //     });
+    // by limiting the path to current directory only
+    const sanitizePath = path.normalize(parsedUrl.pathname).replace(/^(\.\.[\/\\])+/, '');
+    let pathname = path.join(__dirname, sanitizePath);
+
+    fs.exists(pathname, function (exist) {
+        if (!exist) {
+            // if the file is not found, return 404
+            res.statusCode = 404;
+            res.end(`File ${pathname} not found!`);
+            return;
+        }
+
+        // if is a directory, then look for index.html
+        if (fs.statSync(pathname).isDirectory()) {
+            pathname += '/index.html';
+        }
+
+        // read file from file system
+        fs.readFile(pathname, function (err, data) {
+            if (err) {
+                res.statusCode = 500;
+                res.end(`Error getting the file: ${err}.`);
+            } else {
+                // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+                const ext = path.parse(pathname).ext;
+                // if the file is found, set Content-type and send data
+                res.setHeader('Content-type', mimeType[ext] || 'text/plain');
+                res.end(data);
+            }
+        });
+    });
 }
 export {httpApp};
--- src/clients/webReact/index.tsx
+++ src/clients/webReact/index.tsx
@@ -1,9 +1,56 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as io from 'socket.io-client';
+import { Socket } from "socket.io";
+import { TextMessagePostRequest } from "../../lib/messages/messages";
+import { DestinationTypes } from "../../lib/messages/message";
 
 interface HelloProps { compiler: string; framework: string; }
 const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
 
+interface SocketProps {
+    socket:Socket,
+}
+interface TypeingProps {
+    msg:string
+}
+interface ReceivedMsgs{
+    msgs:Array<string>
+}
+class SocketComponent extends React.Component {
+    state: SocketProps & TypeingProps & ReceivedMsgs = {
+        socket:null,
+        msg:"",
+        msgs:[]
+    }
+    constructor(props){
+        super(props);
+    }
+    componentDidMount(){
+        const socket = io('http://localhost/3005');
+        this.state.socket.on("msg",(d)=>{
+            this.state.msgs.push(d)
+        })
+        this.setState({socket});
+    }
+    render = ()=>(
+        <div>
+            <input onChange={(e) => this.setState({ msg:e.target.value})}/>
+            <button onClick={()=>{
+                this.state.socket.send(new TextMessagePostRequest({
+                    body:this.state.msg,
+                    destination:{
+                        type:DestinationTypes.channel,
+                        val:"any"
+                    }
+                }))
+                this.setState({
+                    msgs: [...this.state.msgs, this.state.msg] 
+                })
+            }}>submit</button>
+        </div>
+    )
+}
 document.addEventListener("DOMContentLoaded",()=>{
     ReactDOM.render(
         <Hello compiler="TypeScript" framework="React" />,
